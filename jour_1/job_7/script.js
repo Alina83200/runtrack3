50c2666a8ab52ec3e9a1c909f0164aec95b7ed97
Alina83200 <alina.yefimova@laplateforme.io>
2024-02-12 16:24:58 +0100

    Initialize jour_1\job_7

---
+++ jour_1/job_7/script.js
@@ -0,0 +1,9 @@
+/*Créez une fonction “jourtravaille” qui prend en paramètre une date au format Date. Si la
+date correspond à un jour férié de l’année 2020, la fonction affiche “Le $jour $mois
+$année est un jour férié”. Si elle correspond à un samedi ou un dimanche, alors le
+message affiché est “Non, $jour $mois $année est un week-end”, sinon afficher “Oui,
+$jour $mois $année est un jour travaillé”.
+
+$jour correspond au numéro du jour, $mois au mois et $année à l’année. Les jours fériés
+sont référencés sur
+*/
