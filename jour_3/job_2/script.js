7e1297f8f15469dc333e56633b54434d634e4be6
Alina83200 <alina.yefimova@laplateforme.io>
2024-02-14 16:09:17 +0100

    jour_3:job_2 clone code
    Copy code from the previous task

---
+++ jour_3/job_2/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $("#btn_1").click(function () {
+    $("h3").show();
+  });
+});
+$(document).ready(function () {
+  $("#btn_2").click(function () {
+    $("h3").hide();
+  });
+});
