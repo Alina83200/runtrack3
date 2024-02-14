9c031d9b2f659cc0176bcb84adc777e7acb5694f
Alina83200 <alina.yefimova@laplateforme.io>
2024-02-14 14:51:55 +0100

    Jour_2:Job_5:Script.js add event

--- jour_2/job_5/script.js
+++ jour_2/job_5/script.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    // Визначаємо відсоток прокрутки
+    let scrollPercentage =
+      (document.documentElement.scrollTop /
+        (document.documentElement.scrollHeight - window.innerHeight)) *
+      100;
 
+    // Змінюємо колір футера в залежності від відсотка прокрутки
+    let footer = document.querySelector(".footer");
+    footer.style.backgroundColor = `hsl(${scrollPercentage}, 50%, 50%)`;
+  });
+}); 
