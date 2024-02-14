00b1792d8ff2cbd16edb5d8d480a9c7ef0c8935c
Alina83200 <alina.yefimova@laplateforme.io>
2024-02-14 10:55:17 +0100

    Jour_2:Job_4:Script.js add event

--- jour_2/job_4/script.js
+++ jour_2/job_4/script.js
@@ -1,2 +1,16 @@
+const textarea = document.getElementById("keylogger");
 
 
+
+/*EventTarget метод addEventListener() настраивает функцию, 
+  которая будет вызвана каждый раз, когда указанное событие поставляется цели.
+*/
+
+//Событие keypress происходит когда нажимается символьная клавиша, то есть клавиша которая создаёт символ.
+document.addEventListener("keypress", function (event) {
+  const keyPressed = event.key;
+  if (/^[a-z]$/.test(keyPressed)) {
+    textarea.value +=
+      document.activeElement === textarea ? keyPressed.repeat(2) : keyPressed;
+  }
+});
