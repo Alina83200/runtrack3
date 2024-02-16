49dac1fb7bb69bce54bffad09da68418d63018a0
Alina83200 <alina.yefimova@laplateforme.io>
2024-02-16 11:20:27 +0100

    Jour_3:Job_2:Index.php integrate the game area

---
+++ jour_3/job_2/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Puzzla</title>
+    <link rel="stylesheet" href="puzzle.css">
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="puzzle.js"></script>
+</head>
+
+<body>
+    <br>
+    <div id="board"></div>
+    <h2>Turns: <span id="turns">0</span></h2>
+    <div id="pieces"></div>
+    <button id="refreshButton">Recommencer</button>
+</body>
+
+</html>
