fa99d4c22dff887c749a078efbdd1316518caa57
Alina83200 <alina.yefimova@laplateforme.io>
2024-02-16 13:27:15 +0100

    Jour_3:Job_3:Index.php add game area

---
+++ jour_3/job_3/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id = "board"></div>
+    <h1>Turns: <span id="turns">0</h1>
+    <button id="btn" type="button">Recommencer</button>
+</body>
+</html>
