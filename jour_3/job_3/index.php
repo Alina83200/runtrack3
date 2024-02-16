db3369869ab94327fb2c383d3d1529b3383a527b
Alina83200 <alina.yefimova@laplateforme.io>
2024-02-16 14:48:55 +0100

    Jour_3:Job_3:Puzzle.css Add button style

--- jour_3/job_3/index.php
+++ jour_3/job_3/index.php
@@ -4,10 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="puzzle.css">
+    
 </head>
 <body>
-    <div id = "board"></div>
+
+    <div id ="board"></div>
     <h1>Turns: <span id="turns">0</h1>
-    <button id="btn" type="button">Recommencer</button>
+    <button id="refreshButton" class="custom-btn btn-10" type="button">Recommencer</button>
+
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="puzzle.js"></script>
 </body>
 </html>
