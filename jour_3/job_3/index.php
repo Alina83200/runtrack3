65e5f973d4a041f1645bd8e6f3df7bda778a99bb
Alina83200 <alina.yefimova@laplateforme.io>
2024-02-19 10:35:16 +0100

    Jour_3:Job_3:Index.php add grid

--- jour_3/job_3/index.php
+++ jour_3/job_3/index.php
@@ -1,15 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="puzzle.css">
-    
+
 </head>
+
 <body>
 
-    <div id ="board"></div>
+    <div id="board">
+        <div class="board_content cont_1"><img src="images/2.PNG" alt=""></div>
+        <div class="board_content cont_2"><img src="images/1.PNG" alt=""></div>
+        <div class="board_content cont_3"><img src="images/3.PNG" alt=""></div>
+        <div class="board_content cont_4"><img src="images/4.PNG" alt=""></div>
+        <div class="board_content cont_5"><img src="images/5.PNG" alt=""></div>
+        <div class="board_content cont_6"><img src="images/6.PNG" alt=""></div>
+        <div class="board_content cont_7"><img src="images/7.PNG" alt=""></div>
+        <div class="board_content cont_8"><img src="images/8.PNG" alt=""></div>
+        <div class="board_content cont_9"><img src="images/9.PNG" alt=""></div>
+    </div>
     <h1>Turns: <span id="turns">0</h1>
     <button id="refreshButton" class="custom-btn btn-10" type="button">Recommencer</button>
 
@@ -17,4 +29,5 @@
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="puzzle.js"></script>
 </body>
+
 </html>
